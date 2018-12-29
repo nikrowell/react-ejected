6c96c5709e49e9cd32f05b08a0140ce7e6635b4c
Nik Rowell <nik@nikrowell.com>
2018-12-28 17:10:13 -0700

    TODO comments on Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,6 +6,8 @@ const Button = ({
   children,
   ...props
 }) => (
+  // TODO: add type="button" fallback to props if as === 'button'?
+  // TODO: support for primary boolean prop?
   <Element className={['btn', className].join(' ')} {...props}>
     {children}
   </Element>
